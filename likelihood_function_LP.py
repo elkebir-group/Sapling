0f1848a8de5c24173a908e8e095a1a56bab73860
Yuanyuan Qi <yq7@illinois.edu>
2024-09-09 09:30:23 +0800

    update iterative LP & Simulation comparison

--- likelihood_function_LP.py
+++ likelihood_function_LP.py
@@ -12,30 +12,26 @@ def log_eps(x,eps=1e-6,s_n=3):
     return log(x)
 
 def mll(V,R,T,env):
-    M = np.full_like(V,0.25,dtype=float)
-    Range = 0.25
-    n_intervals = 10
-
-    #build the model for once
-    m,n = V.shape
-    L = M - Range
-    U = M + Range
-    break_points = [[[(1-break_point/n_intervals)*l+break_point/n_intervals*u 
-                      for break_point in range(n_intervals+1) ]
-                     for l,u in zip(ll,uu)]
-                    for ll,uu in zip(L,U)]
-
+    tot_answer = 0.
+    m_,n = V.shape
+    n_intervals = 6
+    m = 1
+    M = np.zeros((1,n),dtype=float)
+    
     model = gp.Model(env=env)
 
-    f_vars = [[model.addVar(vtype=GRB.CONTINUOUS,lb=L[p][i],ub=U[p][i]) 
+    f_vars = [[model.addVar(vtype=GRB.CONTINUOUS,lb=0,ub=1) 
                for i in range(n)] 
               for p in range(m)]
+    
     f_i_vars = [[[model.addVar(vtype=GRB.CONTINUOUS,lb=0,ub=1) 
                   for break_point in range(n_intervals+1)] 
                  for i in range(n)]
                 for p in range(m)]
-
+    
     lin_expr = gp.LinExpr()
+    lin_expr2 = gp.LinExpr()
+    
     for p in range(m):
         for i in T[-1]:
             lin_expr += f_vars[p][i]
@@ -47,58 +43,55 @@ def mll(V,R,T,env):
                 lin_expr += f_vars[p][j]
             model.addConstr(lin_expr<=f_vars[p][i])
             lin_expr.clear()
-
-    lin_expr2 = gp.LinExpr()
+    
     update_constrs = [[None for i in range(n)] for p in range(m)]
     for p in range(m):
         for i in range(n):
             for k in range(n_intervals+1):
                 lin_expr += f_i_vars[p][i][k];
-                lin_expr2 += f_i_vars[p][i][k] * break_points[p][i][k]
+                lin_expr2 += f_i_vars[p][i][k] # * break_points[p][i][k] placeholder only
             model.addConstr(lin_expr == 1)
             update_constrs[p][i] = model.addConstr(lin_expr2 == f_vars[p][i])
             lin_expr.clear()
             lin_expr2.clear()
 
-    for p in range(m):
-        for i in range(n):
-            for k in range(n_intervals+1):
-                lin_expr += f_i_vars[p][i][k] * (V[p][i]*log_eps(break_points[p][i][k])+
-                                          R[p][i]*log_eps(1-break_points[p][i][k]))
-    model.setObjective(lin_expr, GRB.MAXIMIZE)
-    lin_expr.clear()
+    for _ in range(m_):
+        M.fill(0.25)
+        Range = 0.25
+        for __ in range(15):
+            L = M - Range
+            U = M + Range
+            break_points = [[[(1-break_point/n_intervals)*l+break_point/n_intervals*u 
+                              for break_point in range(n_intervals+1) ]
+                             for l,u in zip(ll,uu)]
+                            for ll,uu in zip(L,U)]
 
-    model.optimize()
+            for p in range(m):
+                for i in range(n):
+                    f_vars[p][i].LB=L[p][i]
+                    f_vars[p][i].UB=U[p][i]
 
-    #update model #10 iterations are enough
-    for __ in range(10):
-        Range*=6/n_intervals
-        for p in range(m):
-            for i in range(n):
-                M[p][i] = f_vars[p][i].X
-        L = M - Range
-        U = M + Range
-        for p in range(m):
-            for i in range(n):
-                f_vars[p][i].LB=L[p][i]
-                f_vars[p][i].UB=U[p][i]
+            for p in range(m):
+                for i in range(n):
+                    for k in range(n_intervals+1):
+                        model.chgCoeff(update_constrs[p][i], f_i_vars[p][i][k], newvalue=break_points[p][i][k])
 
+            for p in range(m):
+                for i in range(n):
+                    for k in range(n_intervals+1):
+                        lin_expr += f_i_vars[p][i][k] * (V[_][i]*log_eps(break_points[p][i][k])+
+                                                  R[_][i]*log_eps(1-break_points[p][i][k]))
+            
+            model.setObjective(lin_expr, GRB.MAXIMIZE)
+            
+            lin_expr.clear()
+            model.optimize()
+            Range*=4/n_intervals
+            for p in range(m):
+                for i in range(n):
+                    M[p][i] = f_vars[p][i].X
+                    
+        tot_answer += model.ObjVal
 
-        break_points = [[[(1-break_point/n_intervals)*l+break_point/n_intervals*u 
-                          for break_point in range(n_intervals+1) ]
-                         for l,u in zip(ll,uu)]
-                        for ll,uu in zip(L,U)]
-
-        for p in range(m):
-            for i in range(n):
-                for k in range(n_intervals+1):
-                    model.chgCoeff(update_constrs[p][i], f_i_vars[p][i][k], newvalue=break_points[p][i][k])
-        for p in range(m):
-            for i in range(n):
-                for k in range(n_intervals+1):
-                    lin_expr += f_i_vars[p][i][k] * (V[p][i]*log_eps(break_points[p][i][k])+
-                                              R[p][i]*log_eps(1-break_points[p][i][k]))
-        model.setObjective(lin_expr, GRB.MAXIMIZE)
-        lin_expr.clear()
-        model.optimize()
-    return model.ObjVal
+    return tot_answer
+        
